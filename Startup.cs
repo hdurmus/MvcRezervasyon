9dac6a3297a2c765e4e89ee29a9b7667d744607a
Huseyin Durmus <hdurmus@gmail.com>
2018-02-26 10:01:14 +0300

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UcakBiletiRezervasyon.Startup))]
+namespace UcakBiletiRezervasyon
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
